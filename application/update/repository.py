c45c241c713061c7b09c7acff8b7dcb07ba0e10f
Dominik Geldmacher <dominik.geldmacher@saint-gobain.com>
2022-10-17 10:04:33 +0200

    minimized

--- application/update/repository.py
+++ application/update/repository.py
@@ -38,5 +38,25 @@ def get_classes_from_source(file_content: str) -> list:
     pattern = 'class="(.*)"'
     classes_with_vars = re.findall(pattern, file_content)
     tailwind_classes = [tw_class.replace('-[var(--any-value)]', '') for tw_class in classes_with_vars]
-    log.debug(len(tailwind_classes))
-    return list(sorted(tailwind_classes))
+    minimized = minimize_classes(tailwind_classes)
+    return list(sorted(minimized))
+
+def minimize_classes(tailwind_classes: list, keep_longest_strain: bool = False):
+    """
+    Minimize the list of tailwind classes to remove the classes duplicates and
+    leave it with the stems.
+    """
+    minimized = []
+    for _class in sorted(tailwind_classes):
+        try:
+            stem = _class.split("-")[0]
+        except IndexError:
+            stem = _class
+        if stem in minimized or not stem or not _class:
+            continue
+
+        if keep_longest_strain:
+            minimized.append(stem)
+        else:
+            minimized.append(_class)
+    return minimized
